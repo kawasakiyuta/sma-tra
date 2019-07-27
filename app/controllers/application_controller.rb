4c66e16472d7a6c62bfef2c177c2bb9556d54545
zukupan <y.kawasakicom@gmail.com>
2019-07-27 18:23:45 +0900

    summary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:cust_last_name, :cust_first_name, :gender, :cust_email_address, :phone,:birth_date, :proxy_last_name, :proxy_first_name, :proxy_email_address])
+  end
 end
