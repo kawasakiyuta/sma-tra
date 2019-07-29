5566ea65bf3d2a3d3504ddf9013f6ffa0a11f16c
zukupan <y.kawasakicom@gmail.com>
2019-07-29 14:17:20 +0900

    Route Controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'subscriptions#index'
   devise_for :users
   
-  root to: 'index'
+  resources :users, only: [:index, :edit, :update]
+  resources :subscriptions
+  resources :services
 
 end
