4912f5df472861a81d5144292077ef50f647c79e
zukupan <y.kawasakicom@gmail.com>
2019-07-30 14:34:38 +0900

    マイグレーションファイルの作成
    モデル作成によってマイグレーションファイルを作成

---
+++ db/migrate/20190730052457_create_subscriptions.rb
@@ -0,0 +1,14 @@
+class CreateSubscriptions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :subscriptions do |t|
+      t.integer :user_id,              null: false
+      t.integer :service_id,              null: false
+      t.string :email_address,              null: false
+      t.string :password,              null: false
+      t.string :phone,              
+      t.datetime :last_login_at,              null: false
+      t.integer :in_use,              null: false
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+
+Service.create(:name => '観察日記', :sales_date => '2011-11-14', :price => 1000)
+Service.create(:name => 'Simple Life', :sales_date => '2012-02-08', :price => 2300)
+Service.create(:name => 'メガネ入門', :sales_date => '2011-10-25', :price => 800)
