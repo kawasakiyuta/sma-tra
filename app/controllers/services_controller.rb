d3aff8a11d992f2f6bdb7b32f67e50333605cc32
zukupan <y.kawasakicom@gmail.com>
2019-07-31 14:24:55 +0900

    Update services_controller.rb
    サービス一覧から詳細に入り，新規サブスクリプションを作成できるようにコントローラに記述

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,4 +2,15 @@ class ServicesController < ApplicationController
   def index
     @services = Service.all
   end
+
+  def show
+      user = current_user
+      @subscription = Subscription.new(user_id: user.id, service_id: params[:id])
+      
+      @service = Service.find_by(id: params[:id])
+    
+  end
+
+
+
 end
