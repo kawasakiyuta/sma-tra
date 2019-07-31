0296db58149f5c666f74c1bb2422198507bc0687
zukupan <y.kawasakicom@gmail.com>
2019-08-01 00:38:46 +0900

    Update services_controller.rb
    サービス一覧において，サブスクリプションの登録がある場合はindexへリダイレクト， ない場合は登録画面へ行くように設定

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -5,12 +5,13 @@ class ServicesController < ApplicationController
 
   def show
       user = current_user
-      @subscription = Subscription.new(user_id: user.id, service_id: params[:id])
-      
-      @service = Service.find_by(id: params[:id])
-    
+      @subscription = Subscription.find_by(user_id: user.id, service_id: params[:id])
+      if @subscription
+        redirect_to action: 'index'
+      else
+        @subscription = Subscription.new(user_id: user.id, service_id: params[:id])
+        @service = Service.find_by(id: params[:id])
+      end
   end
-
-
 
 end
