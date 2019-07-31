133f0a342453e7367ed5cb3283cca608c1021674
zukupan <y.kawasakicom@gmail.com>
2019-08-01 00:39:33 +0900

    Update subscriptions_controller.rb
    サブスクリプションの登録，表示，解約手続きができるように記述

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,5 +1,37 @@
 class SubscriptionsController < ApplicationController
   def index
+    user = current_user
+    @subscriptions = user.subscriptions
     
+  end
+
+  def show
+    @subscription = Subscription.find(params[:id])
+  end
+
+  def create
+    @service 
+    @subscription = Subscription.new(subscription_params)
+    @subscription.save
+    user = current_user
+    @subscriptions = user.subscriptions
+    render :index
+  end
+
+  def update
+    subscription = Subscription.find(params[:id])
+    if subscription.user_id == current_user.id
+      subscription.update(subscription_params)
+    end
+    user = current_user
+    @subscriptions = user.subscriptions
+    render :index
+  end
+
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:email_address, :password, :phone, :contract_number, :service_id, :in_use).merge(user_id: current_user.id)
   end
 end
