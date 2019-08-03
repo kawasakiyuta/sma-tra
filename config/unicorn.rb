2981173d0da6832ca1f2f2d38fc0ebcf64ca3d44
zukupan <y.kawasakicom@gmail.com>
2019-08-03 23:57:48 +0900

    Update unicorn.rb
    CentOS7ではnginxとunicornで/tmpディレクトリが共有できない。
    CentOS7ではsocketファイルを/tmp以下に置くのは非推奨で、/var/run以下におくと良いらしい。
    参照）
    https://qiita.com/emahiro/items/b2762a26bca35bbb0cf9

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,8 +2,8 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
-listen '/var/www/sma-tra/tmp/unicorn.sock'
-pid    '/var/www/sma-tra/tmp/unicorn.pid'
+listen '/var/run/unicorn.sock'
+pid    '/var/run/unicorn.pid'
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
